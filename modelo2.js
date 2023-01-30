6e88242ba1b65163cb52e379fa35ee8d0f741a7c
Carlos Eduardo <104845761+carloseduzinn@users.noreply.github.com>
2023-01-30 08:32:12 -0300

    Delete modelo2.js

--- modelo2.js
+++
@@ -1,5 +0,0 @@
-let resultado = ""; 
-        for (let num = 2; num < 51; num++) {
-            document.getElementById("resul").innerHTML = resultado
-            resultado += num *2 + "</br>" 
-        }
